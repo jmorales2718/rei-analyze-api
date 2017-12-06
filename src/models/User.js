8015d3a1aa8f91252cf85d636394fd84a437fc3b
jmorales2718 <john.morales@richmond.edu>
2017-12-05 20:45:20 -0500

    signup, server-side validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
 
 // Todo: add uniqueness and email validation
 const schema = new mongoose.Schema(
 	{
-		email: { type: String, required: true, lowercase: true, index: true },
+		email: {
+			type: String,
+			required: true,
+			lowercase: true,
+			index: true,
+			unique: true
+		},
 		passwordHash: { type: String, required: true }
 	},
 	{ timestamps: true }
@@ -15,12 +22,16 @@ schema.methods.isValidPassword = function isValidPassword(password) {
 	return bcrypt.compareSync(password, this.passwordHash);
 };
 
+schema.methods.setPassword = function setPassword(password) {
+	this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
 schema.methods.generateJWT = function generateJWT() {
 	return jwt.sign(
 		{
 			email: this.email
 		},
-		"secretkey"
+		process.env.JWT_SECRET
 	);
 };
 
@@ -30,5 +41,7 @@ schema.methods.toAuthJSON = function toAuthJSON() {
 		token: this.generateJWT()
 	};
 };
+
+schema.plugin(uniqueValidator, { message: "This email is already taken" });
 
 export default mongoose.model("User", schema);
